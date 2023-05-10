615129a4f31fd1ce55e6e5481e58f94b57377d4e
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-05-10 19:35:44 +0700

    add admin and product_category

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	admin "online-course.mifwar.com/internal/admin/injector"
 	oauth "online-course.mifwar.com/internal/oauth/injector"
+	productCategory "online-course.mifwar.com/internal/product_category/injector"
 	profile "online-course.mifwar.com/internal/profile/injector"
 	register "online-course.mifwar.com/internal/register/injector"
 	mysql "online-course.mifwar.com/pkg/db/mysql"
@@ -17,6 +19,8 @@ func main() {
 	register.InitializedService(db).Route(&r.RouterGroup)
 	oauth.InitializedService(db).Route(&r.RouterGroup)
 	profile.InitializedService(db).Route(&r.RouterGroup)
+	admin.InitializedService(db).Route(&r.RouterGroup)
+	productCategory.InitializedService(db).Route(&r.RouterGroup)
 
 	r.Run()
 }
