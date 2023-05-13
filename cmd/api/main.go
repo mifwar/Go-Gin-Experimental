64bafc683921c0853258c09aef1c0b9749df435d
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-05-13 11:33:30 +0700

    add classRoom, dashboard, order, and webhook

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,10 @@ import (
 	productCategory "online-course.mifwar.com/internal/product_category/injector"
 	profile "online-course.mifwar.com/internal/profile/injector"
 	register "online-course.mifwar.com/internal/register/injector"
+	classRoom "online-course.mifwar.com/internal/class_room/injector"
+	dashboard "online-course.mifwar.com/internal/dashboard/injector"
+	order "online-course.mifwar.com/internal/order/injector"
+	webhook "online-course.mifwar.com/internal/webhook/injector"
 
 	mysql "online-course.mifwar.com/pkg/db/mysql"
 )
@@ -29,6 +33,10 @@ func main() {
 	product.InitializedService(db).Route(&r.RouterGroup)
 	cart.InitializedService(db).Route(&r.RouterGroup)
 	discount.InitializedService(db).Route(&r.RouterGroup)
+	order.InitializedService(db).Route(&r.RouterGroup)
+	webhook.InitializedService(db).Route(&r.RouterGroup)
+	classRoom.InitializedService(db).Route(&r.RouterGroup)
+	dashboard.InitializedService(db).Route(&r.RouterGroup)
 
 	r.Run()
 }
