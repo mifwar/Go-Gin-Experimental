2653bf89d48f344739c5ba0687a901428e4595ee
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-05-13 11:31:59 +0700

    remove unused comma

--- internal/discount/dto/discount_request.go
+++ internal/discount/dto/discount_request.go
@@ -8,7 +8,7 @@ type DiscountRequestBody struct {
 	Quantity          int64     `json:"quantity" binding:"required"`
 	Type              string    `json:"type" binding:"required"`
 	RemainingQuantity int64     `json:"remaining_quantity" binding:"number"`
-	Value             int64     `json:"value" binding:"required, number"`
+	Value             int64     `json:"value" binding:"required,number"`
 	StartDate         time.Time `json:"start_date"`
 	EndDate           time.Time `json:"end_date"`
 	CreatedBy         int64     `json:"created_by"`
